9bfa6f94300f6e48c88e2c5985303c848133f0c4
YeeSoft <makitrataras@gmail.com>
2016-01-10 21:17:09 +0200

    update 0.0.1-rc

--- migrations/m150731_150644_add_page_menu_links.php
+++ migrations/m150731_150644_add_page_menu_links.php
@@ -8,7 +8,7 @@ class m150731_150644_add_page_menu_links extends Migration
     public function up()
     {
         $this->insert('menu_link', ['id' => 'page', 'menu_id' => 'admin-menu', 'link' => '/page/default/index', 'image' => 'file', 'created_by' => 1, 'order' => 2]);
-        $this->insert('menu_link_lang', ['link_id' => 'page', 'label' => 'Pages', 'language' => 'en']);
+        $this->insert('menu_link_lang', ['link_id' => 'page', 'label' => 'Pages', 'language' => 'en-US']);
     }
 
     public function down()
